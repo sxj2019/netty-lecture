fddd7c5118e8c68dffacf5042f0bf5b8e644d9a2
sxj <1010861233@qq.com>
2019-09-18 18:25:15 +0800

    websocket

---
+++ src/main/java/com/imooc/fiveClass/server/WbServerInitializer.java
@@ -0,0 +1,27 @@
+package com.imooc.fiveClass.server;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * @program: netty-lecture
+ * @description:
+ * @author: sxj
+ * @create: 2019-09-18 17:20
+ **/
+public class WbServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpObjectAggregator(8192));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+        pipeline.addLast(new WbServerHandler());
+    }
+}
